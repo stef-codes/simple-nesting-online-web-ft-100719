c6e2db83fb4b94902341c6607232db2592792bc3
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-12 06:18:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  language = programmer_hash[:dennis_ritchie][:languages]
-  return language[0]
+  programmer_hash[:dennis_ritchie][:languages][0]
+ # return language[0]
 end
 
 def adding_matz
